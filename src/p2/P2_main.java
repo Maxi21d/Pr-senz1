f28100a9338ec392101f875e37ac58b93d2d146f
Maxi <maxidecaro650@gmail.com>
2025-10-30 13:11:14 +0100

    .

---
+++ src/p2/P2_main.java
@@ -0,0 +1,14 @@
+package p2;
+
+public class P2_main {
+    public static void main(String[] args) {
+        int i = 6;
+        boolean b = true;
+        if (i > 20) {
+            b = false;
+        }
+        System.out.println(b);
+
+
+    }
+}
