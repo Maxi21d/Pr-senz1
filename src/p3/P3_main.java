eb695a911ffa5f4b9d4b45d1dcce3716c4bb6bcd
Maxi <maxidecaro650@gmail.com>
2025-10-31 11:38:59 +0100

    .

---
+++ src/p3/P3_main.java
@@ -0,0 +1,16 @@
+package p3;
+
+public class P3_main {
+    public static void main(String[] args) {
+        int i = 9;
+        int j = 2;
+        int k = 3;
+        if (i > j) {
+            System.out.println("A");
+        }
+        if (i > k) {
+            System.out.println("B");
+        }
+
+    }
+}
