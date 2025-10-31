34844e1e51083b9f39fb3454b3adc087c6bd5794
Maxi <maxidecaro650@gmail.com>
2025-10-31 13:53:30 +0100

    .

--- src/h2/H2_main.java
+++ src/h2/H2_main.java
@@ -1,7 +1,33 @@
 package h2;
 
 public class H2_main {
-    public static void main(String[] args) {
-        int
+        public static void main(String[] args) {
+            int i = 2, j = 4, k = 3;
+            int min = i; // i ist der Startwert
+            int max = i; // i ist der Startwert
+
+            if (j < min) {
+                min = j;
+            }
+            if (k < min) {
+                min = k;
+            }
+
+            if (j > max) {
+                max = j;
+            }
+            if (k > max) {
+                max = k;
+            }
+
+            System.out.println("Minimum: " + min);
+            System.out.println("Maximum: " + max);
+        }
     }
-}
+
+
+
+
+
+
+
